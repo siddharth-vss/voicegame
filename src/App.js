f7537f22ad799b043427980ce4de479536b9b1ba
Siddharth <siddharth@vasantsoftwaressolution.com>
2023-12-15 18:53:55 +0530

    add voice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// const App = () => {
+
+import { all } from "q";
+
+ 
+// const SpeechRecognition = 
+// window.SpeechRecognition || window.webkitSpeechRecognition;
+
+//   return (
+//    <>
+//      <div>
+//        Hello 
+//      </div>
+//      <button >start</button>
+//    </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+const App = () => {
+
+  let mst=`  he prabhu ,.
+  he hari ram!. krishna jaganath premanand .
+  ye kya hua ?
+  gamcha bhi khul gaya.
+`;
+
+  var msg = new SpeechSynthesisUtterance(mst);
+  const allVoice = speechSynthesis.getVoices();
+  console.log(allVoice); 
+  msg.voice = allVoice[12];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+     <div>
+       Hello 
+     </div>
+     <button onClick={()=>
+    window.speechSynthesis.speak(msg)}>start</button>
+   </>
+  )
 }
 
-export default App;
+export default App
